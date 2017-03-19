6e5196f7669e82ec44a2d8f1d1ee19f4c4c54f55
JellyBrick <shlee1503@naver.com>
2017-03-19 22:39:27 +0900

    Code optimization

--- src/main/java/nxlogin/data/UserData.java
+++ src/main/java/nxlogin/data/UserData.java
@@ -26,10 +26,12 @@ public class UserData {
 	}
 
 	/*
-	 * public LinkedHashMap<String, Object> configSet(Config config) {
-	 * LinkedHashMap<String, Object> map = new LinkedHashMap<>(); map =
-	 * (LinkedHashMap<String, Object>) config.getAll(); return map; }
-	 */
+	public LinkedHashMap<String, Object> configSet(Config config) {
+		LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+		map = (LinkedHashMap<String, Object>) config.getAll();
+		return map;
+	}
+	*/
 
 	public void save() {
 		Config password = new Config(new File(Main.getInstance().getDataFolder(), "password.json"), Config.JSON);
@@ -49,36 +51,43 @@ public class UserData {
 	}
 
 	public boolean register(String user, String password) {
-		Player guest;
-		if (!Main.unLogins.contains(guest = Server.getInstance().getPlayer(user))) {
+		Player guest = Server.getInstance().getPlayer(user);
+		if (!Main.unLogins.contains(guest)) {
 			Server.getInstance().getPlayer(user).sendMessage(Main.command("당신은 이미 로그인 되어있습니다"));
 			return false;
 		} else if (password.length() < 5) {
-			guest.sendMessage(Main.alert("회원가입이 정상적으로 이루어지지 않았습니다 §c사유: 비밀번호의 길이는 최소 다섯글자입니다"));
+			guest.sendMessage(Main.alert("회원가입이 정상적으로 이루어지지 않았습니다.\n §c사유: 비밀번호의 길이는 최소 다섯글자입니다"));
 			return false;
 		} else if (isRegisterUser(user)) {
-			guest.sendMessage(Main.alert("당신은 이미 회원가입정보가 있습니다"));
+			guest.sendMessage(Main.alert("이미 가입되어 있는 계정입니다."));
+			guest.sendMessage(Main.alert("서버에 처음 접속하셨다면, 다른 닉네임으로 들어와 주세요."));
 			return false;
+		} else {
+			UserData.password.put(user.toLowerCase(), password.toLowerCase());
+			UserData.address.put(user.toLowerCase(), guest.getAddress());
+			guest.sendMessage(Main.success(new StringBuilder("회원가입이 성공적으로 이루어졌습니다 §b:: 비밀번호: §l")
+					.append(password.toLowerCase()).toString()));
+			return true;
 		}
-		UserData.password.put(user.toLowerCase(), password.toLowerCase());
-		UserData.address.put(user.toLowerCase(), guest.getAddress());
-		guest.sendMessage(Main.success("회원가입이 성공적으로 이루어졌습니다 §b:: §l{pw}").replace("{pw}", password.toLowerCase()));
-		return true;
 	}
 
 	public boolean ipLogin(Player user) {
 		if (isRegisterUser(user.getName())) {
 			if (user.getAddress().contains(getLastIp(user.getName()))) {
-				user.sendMessage(Main.success("마지막으로 로그인한 아이피로 들어오셨습니다 §b:: §l자동로그인 되었습니다"));
+				user.sendMessage(Main.success("마지막으로 로그인된 아이피로 들어오셨습니다 §b:: §l자동로그인 되었습니다"));
+				user.sendMessage(Main.success(new StringBuilder("마지막 로그인 아이피§b:: §lIP : ").append(this.getLastIp(user.getName())).toString()));
 				Main.unLogins.remove(user);
 				return true;
 			} else {
-				user.sendMessage(Main.command("자동로그인이 성공하지 못했습니다 §6:: §l채팅창에 비밀번호를 입력하여 로그인해주세요"));
+				user.sendMessage(Main.command("자동로그인이 성공하지 못했습니다 §6:: §l채팅창에 비밀번호를 입력하시거나,"));
+				user.sendMessage(Main.command("§6§l/로그인 비밀번호 를 입력해주세요."));
 				return false;
 			}
 		}
-		user.sendMessage(Main.command("자동로그인이 성공하지 못했습니다 §6:: §l/회원가입 <설정할 비밀번호> 명령어를 통해 회원가입해주세요"));
-		return false;
+		else {
+			user.sendMessage(Main.command("§6§l/회원가입 <설정할 비밀번호> 명령어를 통해 회원가입해주세요"));
+			return false;
+		}
 	}
 
 	public boolean passwordLogin(Player user, String password) {
@@ -86,26 +95,26 @@ public class UserData {
 			if (!Main.unLogins.contains(user)) {
 				user.sendMessage(Main.command("당신은 이미 로그인 되어있습니다"));
 				return true;
-			} else if (((String) UserData.password.get(user.getName().toLowerCase())).equals(password.toLowerCase())){
+			} else if (UserData.password.get(user.getName().toLowerCase()).equals(password.toLowerCase())) {
 				update(user.getName(), password, user.getAddress());
 				Main.unLogins.remove(user);
-				user.sendMessage(Main.success("성공적으로 로그인되었습니다 §b:: §l IP: {ip}").replace("{ip}", user.getAddress()));
+				user.sendMessage(Main.success(new StringBuilder("성공적으로 로그인되었습니다. §b:: §l IP: ")
+						.append(user.getAddress()).toString()));
 				return true;
-			}else{
-				user.sendMessage(Main.alert("로그인에 성공하지 못했습니다 §c:: §l비밀번호가 옳지 않습니다"));
+			} else {
+				user.sendMessage(Main.alert("로그인에 성공하지 못했습니다. §c:: §l비밀번호가 옳지 않습니다"));
 				return false;
 			}
 		} else {
-			user.sendMessage(Main.command("계정정보가 존재하지 않습니다 회원가입을 진행하여주세요§6 :: §l/회원가입 <설정할 비밀번호>"));
+			user.sendMessage(Main.command("계정정보가 존재하지 않습니다. 회원가입을 진행하여 주세요.§6 :: §l/회원가입 <설정할 비밀번호>"));
 			return false;
 		}
-		
+
 	}
 
-	public void update(String user,String password,String ip){
+	public void update(String user, String password, String ip) {
 		UserData.address.put(user.toLowerCase(), ip);
 		UserData.password.put(user.toLowerCase(), password);
-		return;
 	}
 
 	public void unregister(String user) {
@@ -114,8 +123,7 @@ public class UserData {
 	}
 
 	public String getLastIp(String user) {
-		return (address.get(user.toLowerCase()) + "");
+		return (String) address.get(user.toLowerCase());
 	}
-
 
 }
--- src/main/java/nxlogin/tasks/UnLoginPlayerTask.java
+++ src/main/java/nxlogin/tasks/UnLoginPlayerTask.java
@@ -14,17 +14,17 @@ public class UnLoginPlayerTask extends PluginTask<Main> {
 	public void onRun(int currentTick) {
 		if (Main.unLogins.isEmpty()) {
 			return;
-		}
-		for (Player p : Main.unLogins) {
-			
-			if (p.getPosition().equals(owner.getServer().getDefaultLevel().getSpawnLocation())) {
-				p.sendTip(Main.alert("로그인 후 서버를 플레이해주세요"));
-				return;
+		} else {
+			for (Player p : Main.unLogins) {
+				if (p.getPosition().equals(owner.getServer().getDefaultLevel().getSpawnLocation())) {
+					p.sendTip(Main.alert("로그인 후 서버를 플레이해주세요"));
+					return;
+				} else {
+					p.sendTip(Main.alert("로그인 후 서버를 플레이해주세요"));
+					p.teleport(owner.getServer().getDefaultLevel().getSpawnLocation());
+					p.sendMessage(Main.alert("회원가입이나 로그인 후 서버를 플레이 해 주세요"));
+				}
 			}
-			p.sendTip(Main.alert("로그인 후 서버를 플레이해주세요"));
-			p.teleport(owner.getServer().getDefaultLevel().getSpawnLocation());
-			p.sendMessage(Main.alert("회원가입이나 로그인 후 서버를 플레이 해 주세요"));
-
 		}
 
 	}
